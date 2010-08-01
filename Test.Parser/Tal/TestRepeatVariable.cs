2dce5cdcac44fbbd73ee8b77d954a3ef8e98237e
Craig Fowler <craig@craigfowler.me.uk>
2010-08-01 21:32:56 +0100

    Finished RepeatVariable.GenerateLetterReference(int) and its private overload.

--- Test.Parser/Tal/TestRepeatVariable.cs
+++ Test.Parser/Tal/TestRepeatVariable.cs
@@ -14,11 +14,12 @@ namespace Test.CraigFowler.Web.ZPT.Tal
     {
       RepeatVariable test = new RepeatVariable(new List<int>());
       
-      Assert.AreEqual("a",  test.GenerateLetterReference(0),   "Letter reference for 0");
-      Assert.AreEqual("g",  test.GenerateLetterReference(6),   "Letter reference for 6");
-      Assert.AreEqual("aa", test.GenerateLetterReference(26),  "Letter reference for 26");
-      Assert.AreEqual("ab", test.GenerateLetterReference(27),  "Letter reference for 27");
-      Assert.AreEqual("tg", test.GenerateLetterReference(500), "Letter reference for 500");
+      Assert.AreEqual("a",   test.GenerateLetterReference(0),    "Letter reference for 0");
+      Assert.AreEqual("g",   test.GenerateLetterReference(6),    "Letter reference for 6");
+      Assert.AreEqual("aa",  test.GenerateLetterReference(26),   "Letter reference for 26");
+      Assert.AreEqual("ab",  test.GenerateLetterReference(27),   "Letter reference for 27");
+      Assert.AreEqual("sg",  test.GenerateLetterReference(500),  "Letter reference for 500");
+      Assert.AreEqual("dkk", test.GenerateLetterReference(3000), "Letter reference for 3000");
     }
   }
 }
