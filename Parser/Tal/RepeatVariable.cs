74d138029846fa6a31c143b259169446e9544be5
Craig Fowler <craig@craigfowler.me.uk>
2010-08-14 14:01:43 +0100

    Completed the RepeatVariable class and all unit tests are written

--- Parser/Tal/RepeatVariable.cs
+++ Parser/Tal/RepeatVariable.cs
@@ -27,7 +27,7 @@ using System.Text;
 namespace CraigFowler.Web.ZPT.Tal
 {
   /// <summary>
-  /// <para>Represents a repeat variable for using with a TAL 'repeat' directive.</para>
+  /// <para>Represents a repeat variable for use with a TAL 'repeat' directive.</para>
   /// </summary>
   public class RepeatVariable : IEnumerator
   {
@@ -35,9 +35,6 @@ namespace CraigFowler.Web.ZPT.Tal
     
     private const string
       ALPHABET                = "abcdefghijklmnopqrstuvwxyz";
-    
-    private const int
-      ALPHABET_LENGTH         = 26;
     
     #endregion
     
@@ -90,6 +87,7 @@ namespace CraigFowler.Web.ZPT.Tal
     public bool Even
     {
       get {
+        CheckThatIndexIsInRange();
         return Math.Round((float) this.Index / 2f) == ((float) this.Index / 2f);
       }
     }
@@ -115,6 +113,7 @@ namespace CraigFowler.Web.ZPT.Tal
     public bool Start
     {
       get {
+        CheckThatIndexIsInRange();
         return this.Index == 0;
       }
     }
@@ -129,6 +128,7 @@ namespace CraigFowler.Web.ZPT.Tal
     public bool End
     {
       get {
+        CheckThatIndexIsInRange();
         return this.Index == (this.Length - 1);
       }
     }
@@ -154,6 +154,7 @@ namespace CraigFowler.Web.ZPT.Tal
     public string Letter
     {
       get {
+        CheckThatIndexIsInRange();
         return GenerateLetterReference(this.Index);
       }
     }
@@ -185,16 +186,7 @@ namespace CraigFowler.Web.ZPT.Tal
     public object Current
     {
       get {
-        if(this.Index < 0)
-        {
-          throw new InvalidOperationException("The index is currently positioned before the start of the collection. " +
-                                              "Have you forgotten to use 'MoveNext()'?");
-        }
-        else if(this.Index >= this.Length)
-        {
-          throw new InvalidOperationException("The index is currently positioned beyond the end of the collection.");
-        }
-        
+        CheckThatIndexIsInRange();
         return this.UnderlyingCollection[this.Index];
       }
     }
@@ -228,25 +220,14 @@ namespace CraigFowler.Web.ZPT.Tal
     /// <para>Moves the <see cref="Index"/> to the next item in the collection.</para>
     /// </summary>
     /// <returns>
-    /// A <see cref="System.Boolean"/>, if there is another item within the collection then this returns true.  If this
-    /// action would place <see cref="Index"/> past the end of the <see cref="UnderlyingCollection"/> then false is
-    /// returned.
+    /// A <see cref="System.Boolean"/>, indicating whether there was another item within the
+    /// <see cref="UnderlyingCollection"/>.  If this action would have placed <see cref="Index"/> past the end of the
+    /// underlying collection then this method returns false.
     /// </returns>
     public bool MoveNext()
     {
-      bool output;
-      
-      if(this.Number < this.Length)
-      {
-        this.Index ++;
-        output = true;
-      }
-      else
-      {
-        output = false;
-      }
-      
-      return output;
+      this.Index ++;
+      return this.Index < this.Length;
     }
     
     /// <summary>
@@ -255,10 +236,83 @@ namespace CraigFowler.Web.ZPT.Tal
     /// <remarks>
     /// <para>This method signature is only included as it is required by <see cref="IEnumerator"/>.</para>
     /// </remarks>
+    /// <exception cref="NotSupportedException">
+    /// This method will ALWAYS throw an exception.  It should not be used.
+    /// </exception>
     public void Reset()
     {
       throw new NotSupportedException("Resetting the repeat variable is not supported");
     }
+    
+    /// <summary>
+    /// <para>
+    /// Performs a sanity-check that the <see cref="Index"/> is in range.  That is, it is not less than zero and that it
+    /// is less than the length of the <see cref="UnderlyingCollection"/>.
+    /// </para>
+    /// </summary>
+    private void CheckThatIndexIsInRange()
+    {
+      if(this.Index < 0)
+      {
+        throw new InvalidOperationException("The index is currently positioned before the start of the collection. " +
+                                            "Have you forgotten to use 'MoveNext()'?");
+      }
+      else if(this.Index >= this.Length)
+      {
+        throw new InvalidOperationException("The index is currently positioned beyond the end of the collection.");
+      }
+    }
+    
+    #endregion
+    
+    #region constructor
+    
+    /// <summary>
+    /// <para>Instantiates this instance with a given <paramref name="list"/>.</para>
+    /// </summary>
+    /// <param name="list">
+    /// An <see cref="IList"/> that will be used as the <see cref="UnderlyingCollection"/> for this instance.
+    /// </param>
+    /// <exception cref="ArgumentNullException">
+    /// If the <paramref name="list"/> is null.
+    /// </exception>
+    public RepeatVariable(IList list)
+    {
+      this.UnderlyingCollection = list;
+      this.Index = -1;
+    }
+    
+    /// <summary>
+    /// <para>Instantiates this instance with a given <paramref name="list"/>.</para>
+    /// </summary>
+    /// <param name="list">
+    /// A <see cref="System.Object"/> that shall be used as the <see cref="UnderlyingCollection"/> for this instance.
+    /// This parameter must be convertable into <see cref="IList"/>.
+    /// </param>
+    /// <exception cref="ArgumentNullException">
+    /// If the <paramref name="list"/> is null.
+    /// </exception>
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// If the <paramref name="list"/> could not be converted to <see cref="IList"/>.
+    /// </exception>
+    public RepeatVariable(object list)
+    {
+      if(list == null)
+      {
+        throw new ArgumentNullException("list");
+      }
+      else if(!(list is IList))
+      {
+        throw new ArgumentOutOfRangeException("list", "List could not be converted to System.Collections.IList.");
+      }
+      
+      this.UnderlyingCollection = list as IList;
+      this.Index = -1;
+    }
+    
+    #endregion
+    
+    #region static methods
     
     /// <summary>
     /// <para>Overloaded.  Generates a letter reference for a given <paramref name="numericIndex"/>.</para>
@@ -273,7 +327,7 @@ namespace CraigFowler.Web.ZPT.Tal
     /// A <see cref="System.String"/> derived from the <paramref name="numericIndex"/> using the scheme <c>a</c> to
     /// <c>z</c> and then <c>aa</c> to <c>az</c>, <c>ba</c> to <c>bz</c> and so on.
     /// </returns>
-    public string GenerateLetterReference(int numericIndex)
+    public static string GenerateLetterReference(int numericIndex)
     {
       StringBuilder output = new StringBuilder();
       
@@ -319,9 +373,10 @@ namespace CraigFowler.Web.ZPT.Tal
     /// </code>
     /// </example>
     /// </remarks>
-    private void GenerateLetterReference(int numericIndex, int characterPosition, ref StringBuilder output)
+    private static void GenerateLetterReference(int numericIndex, int characterPosition, ref StringBuilder output)
     {
       double character, truncatedIndex;
+      int alphabetLength = ALPHABET.Length;
       
       // Sanity-check our input
       if(numericIndex < 0)
@@ -339,8 +394,8 @@ namespace CraigFowler.Web.ZPT.Tal
       
       /* Calculate the truncated index for this value of characterPosition.
        * 
-       * We divide numericIndex by ALPHABET_LENGTH to the power of the characterPosition and then discard
-       * any digits after the decimal point.  This both normalises the numericIndex to the context of the
+       * We divide numericIndex by ([the length of the alphabet] to the power of [the characterPosition]) and then
+       * discard any digits after the decimal point.  This both normalises the numericIndex to the context of the
        * characterPosition and also discards any irrelevant information that would have already been handled in a
        * previous iteration of this method.
        * 
@@ -348,9 +403,9 @@ namespace CraigFowler.Web.ZPT.Tal
        * (which is 26 squared).  Say numericIndex is 3000 then the final result should be "dkk".  To get the "d" we
        * only care about the part of numericIndex that is a multiple of 676.
        */
-      truncatedIndex = Math.Truncate((double) numericIndex / Math.Pow(ALPHABET_LENGTH, characterPosition));
+      truncatedIndex = Math.Truncate((double) numericIndex / Math.Pow(alphabetLength, characterPosition));
       
-      /* On any run of this method beyond the first "a" is at position 1 of the truncatedIndex.  On the first run, "a"
+      /* On any run of this method beyond the first, "a" is at position 1 of the truncatedIndex.  On the first run, "a"
        * is at position zero.  This is because the alphabet has no character to represent zero.  What we do is subtract
        * 1 from the truncated index on any run after the first (IE: for characterPosition > 0).
        */
@@ -362,30 +417,14 @@ namespace CraigFowler.Web.ZPT.Tal
       /* The index of the character we want is the remainder of the truncatedIndex divided by the length of the
        * alphabet.
        */
-      character = truncatedIndex % ALPHABET_LENGTH;
+      character = truncatedIndex % alphabetLength;
       output.Insert(0, ALPHABET[(int) character]);
       
       // Here we decide if we need to recurse and run another iteration.
-      if(truncatedIndex >= ALPHABET_LENGTH)
+      if(truncatedIndex >= alphabetLength)
       {
         GenerateLetterReference(numericIndex, characterPosition + 1, ref output);
       }
-    }
-    
-    #endregion
-    
-    #region constructor
-    
-    /// <summary>
-    /// <para>Instantiates this instance with a given <paramref name="list"/>.</para>
-    /// </summary>
-    /// <param name="list">
-    /// An <see cref="IList"/> that will be used as the <see cref="UnderlyingCollection"/> for this instance.
-    /// </param>
-    public RepeatVariable(IList list)
-    {
-      this.UnderlyingCollection = list;
-      this.Index = -1;
     }
     
     #endregion
