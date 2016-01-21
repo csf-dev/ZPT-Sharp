d76e1baefeb0652209004eb2d4df954c017d0fd2
Craig Fowler <craig@csf-dev.com>
2016-01-21 21:25:14 +0000

    Add unit test and implementation for TAL 'attributes' handler

--- CSF.Zpt/Tal/AttributesAttributeHandler.cs
+++ CSF.Zpt/Tal/AttributesAttributeHandler.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using CSF.Zpt.Rendering;
+using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace CSF.Zpt.Tal
 {
@@ -8,6 +10,21 @@ namespace CSF.Zpt.Tal
   /// </summary>
   public class AttributesAttributeHandler : IAttributeHandler
   {
+    #region constants
+
+    private const string
+      ATTRIBUTE_PATTERN   = @"^(?:(?:([^:]+):)?([^ ]+) )([^ ]+)$",
+      ITEM_PATTERN        = @"((?:[^;]|;;)+)\s*(?:;(?!;))?\s*",
+      ESCAPED_SEMICOLON   = ";;",
+      SEMICOLON           = ";";
+
+    private static readonly Regex
+      Attribute           = new Regex(ATTRIBUTE_PATTERN, RegexOptions.Compiled),
+      ItemMatcher         = new Regex(ITEM_PATTERN, RegexOptions.Compiled);
+
+
+    #endregion
+
     #region methods
 
     /// <summary>
@@ -18,8 +35,107 @@ namespace CSF.Zpt.Tal
     /// <param name="model">Model.</param>
     public AttributeHandlingResult Handle(ZptElement element, Model model)
     {
-      // TODO: Write this implementation
-      throw new NotImplementedException();
+      if(element == null)
+      {
+        throw new ArgumentNullException("element");
+      }
+      if(model == null)
+      {
+        throw new ArgumentNullException("model");
+      }
+
+      var attrib = element.GetTalAttribute(ZptConstants.Tal.AttributesAttribute);
+
+      if(attrib != null)
+      {
+        var itemMatches = ItemMatcher
+          .Matches(attrib.Value)
+          .Cast<Match>()
+          .Select(x => Attribute.Match(x.Groups[1].Value));
+
+        if(itemMatches.Any(x => !x.Success))
+        {
+          string message = String.Format(Resources.ExceptionMessages.ZptAttributeParsingError,
+                                         ZptConstants.Tal.DefaultPrefix,
+                                         ZptConstants.Tal.AttributesAttribute,
+                                         attrib.Value);
+          throw new ParserException(message) {
+            SourceElementName = element.Name,
+            SourceAttributeName = attrib.Name,
+            SourceAttributeValue = attrib.Value
+          };
+        }
+
+        var items = itemMatches
+          .Select(x => new {  Prefix = x.Groups[1].Value,
+                              AttributeName = x.Groups[2].Value,
+                              Expression = x.Groups[3].Value })
+          .ToArray();
+
+        foreach(var item in items)
+        {
+          ExpressionResult result;
+          var unescapedExpression = this.UnescapeSemicolons(item.Expression);
+
+          try
+          {
+            result = model.Evaluate(unescapedExpression, element);
+          }
+          catch(Exception ex)
+          {
+            string message = String.Format(Resources.ExceptionMessages.ExpressionEvaluationException,
+                                           ZptConstants.Tal.DefaultPrefix,
+                                           ZptConstants.Tal.AttributesAttribute,
+                                           item.Expression);
+            throw new ModelEvaluationException(message, ex) {
+              ExpressionText = item.Expression
+            };
+          }
+
+          if(!result.CancelsAction)
+          {
+            if(result.Result == null)
+            {
+              if(String.IsNullOrEmpty(item.Prefix))
+              {
+                element.RemoveAttribute(item.AttributeName);
+              }
+              else
+              {
+                element.RemoveAttribute(item.Prefix, item.AttributeName);
+              }
+            }
+            else
+            {
+              if(String.IsNullOrEmpty(item.Prefix))
+              {
+                element.SetAttribute(item.AttributeName, result.Result.ToString());
+              }
+              else
+              {
+                element.SetAttribute(item.Prefix, item.AttributeName, result.Result.ToString());
+              }
+            }
+          }
+        }
+      }
+
+      return new AttributeHandlingResult(new [] { element }, true);
+    }
+
+    /// <summary>
+    /// Unescapes doubled-up semicolons into singular semicolons.
+    /// </summary>
+    /// <returns>The unescaped string.</returns>
+    /// <param name="expression">A source expression which may contain escaped semicolons.</param>
+    private string UnescapeSemicolons(string expression)
+    {
+      if(expression == null)
+      {
+        throw new ArgumentNullException("expression");
+      }
+
+      return expression.Replace(ESCAPED_SEMICOLON, SEMICOLON);
     }
 
     #endregion
---
+++ Test.CSF.Zpt/Tal/TestAttributesAttributeHandler.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using NUnit.Framework;
+using CSF.Zpt.Tal;
+using CSF.Zpt.Rendering;
+using Ploeh.AutoFixture;
+using Moq;
+using Test.CSF.Zpt.Util;
+using Test.CSF.Zpt.Util.Autofixture;
+using CSF.Zpt;
+
+namespace Test.CSF.Zpt.Tal
+{
+  [TestFixture]
+  public class TestAttributesAttributeHandler
+  {
+    #region fields
+
+    private IFixture _autofixture;
+    private Mock<ZptElement> _element;
+    private DummyModel _model;
+
+    private AttributesAttributeHandler _sut;
+
+    #endregion
+
+    #region setup
+
+    [SetUp]
+    public void Setup()
+    {
+      _autofixture = new Fixture();
+      new DummyModelCustomisation().Customize(_autofixture);
+
+      _model = _autofixture.Create<DummyModel>();
+      _element = new Mock<ZptElement>() { CallBase = true };
+
+      _sut = new AttributesAttributeHandler();
+    }
+
+    #endregion
+
+    #region tests
+
+    [Test]
+    public void TestHandleNoAttribute()
+    {
+      // Arrange
+      _element
+        .Setup(x => x.GetAttribute(ZptConstants.Tal.Namespace,
+                                   ZptConstants.Tal.DefaultPrefix,
+                                   ZptConstants.Tal.AttributesAttribute))
+        .Returns((ZptAttribute) null);
+
+      // Act
+      var result = _sut.Handle(_element.Object, _model);
+
+      // Assert
+      Assert.NotNull(result, "Result nullability");
+      Assert.AreEqual(1, result.Elements.Length, "Count of results");
+      Assert.AreSame(_element.Object, result.Elements[0], "Correct element returned");
+      _element.Verify(x => x.SetAttribute(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()),
+                      Times.Never());
+      _element.Verify(x => x.SetAttribute(It.IsAny<string>(), It.IsAny<string>()),
+                      Times.Never());
+      _element.Verify(x => x.RemoveAttribute(It.IsAny<string>(), It.IsAny<string>()),
+                      Times.Never());
+      _element.Verify(x => x.RemoveAttribute(It.IsAny<string>()),
+                      Times.Never());
+    }
+
+    //        Value               Prefix      Name      Expression
+    //        -----               ------      ----      ----------
+    [TestCase("foo bar",          null,       "foo",    "bar")]
+    [TestCase("pre:foo bar",      "pre",      "foo",    "bar")]
+    [TestCase("pre:foo bar/baz",  "pre",      "foo",    "bar/baz")]
+    public void TestHandleSingleAttribute(string value,
+                                          string expectedPrefix,
+                                          string expectedName,
+                                          string expectedExpression)
+    {
+      // Arrange
+      _element
+        .Setup(x => x.GetAttribute(ZptConstants.Tal.Namespace,
+                                   ZptConstants.Tal.DefaultPrefix,
+                                   ZptConstants.Tal.AttributesAttribute))
+        .Returns(Mock.Of<ZptAttribute>(a => a.Value == value));
+      
+      var expressionResult = _autofixture.Create<string>();
+      Mock.Get(_model)
+        .Setup(x => x.Evaluate(expectedExpression, _element.Object))
+        .Returns(new ExpressionResult(expressionResult));
+
+      // Act
+      var result = _sut.Handle(_element.Object, _model);
+
+      // Assert
+      Assert.NotNull(result, "Result nullability");
+      Assert.AreEqual(1, result.Elements.Length, "Count of results");
+      Assert.AreSame(_element.Object, result.Elements[0], "Correct element returned");
+      Mock.Get(_model).Verify(x => x.Evaluate(expectedExpression, _element.Object), Times.Once());
+      _element.Verify(x => x.SetAttribute(expectedPrefix, expectedName, expressionResult), Times.Once());
+      _element.Verify(x => x.RemoveAttribute(It.IsAny<string>(), It.IsAny<string>()),
+                      Times.Never());
+      _element.Verify(x => x.RemoveAttribute(It.IsAny<string>()),
+                      Times.Never());
+    }
+
+    [TestCase(null, "foo", "bar")]
+    [TestCase("pre", "foo", "bar")]
+    public void TestHandleNullAttribute(string prefix, string name, string expression)
+    {
+      // Arrange
+      string attributeValue = (prefix != null)? String.Format("{0}:{1} {2}", prefix, name, expression) : String.Format("{0} {1}", name, expression);
+      _element
+        .Setup(x => x.GetAttribute(ZptConstants.Tal.Namespace,
+                                   ZptConstants.Tal.DefaultPrefix,
+                                   ZptConstants.Tal.AttributesAttribute))
+        .Returns(Mock.Of<ZptAttribute>(a => a.Value == attributeValue));
+
+      Mock.Get(_model)
+        .Setup(x => x.Evaluate(expression, _element.Object))
+        .Returns(new ExpressionResult(null));
+
+      // Act
+      var result = _sut.Handle(_element.Object, _model);
+
+      // Assert
+      Assert.NotNull(result, "Result nullability");
+      Assert.AreEqual(1, result.Elements.Length, "Count of results");
+      Assert.AreSame(_element.Object, result.Elements[0], "Correct element returned");
+      Mock.Get(_model).Verify(x => x.Evaluate(expression, _element.Object), Times.Once());
+      _element.Verify(x => x.RemoveAttribute(prefix, name), Times.Once());
+      _element.Verify(x => x.SetAttribute(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()),
+                      Times.Never());
+      _element.Verify(x => x.SetAttribute(It.IsAny<string>(), It.IsAny<string>()),
+                      Times.Never());
+    }
+
+    [TestCase(null, "foo", "bar")]
+    [TestCase("pre", "foo", "bar")]
+    public void TestHandleCancelsAction(string prefix, string name, string expression)
+    {
+      // Arrange
+      string attributeValue = (prefix != null)? String.Format("{0}:{1} {2}", prefix, name, expression) : String.Format("{0} {1}", name, expression);
+      _element
+        .Setup(x => x.GetAttribute(ZptConstants.Tal.Namespace,
+                                   ZptConstants.Tal.DefaultPrefix,
+                                   ZptConstants.Tal.AttributesAttribute))
+        .Returns(Mock.Of<ZptAttribute>(a => a.Value == attributeValue));
+
+      Mock.Get(_model)
+        .Setup(x => x.Evaluate(expression, _element.Object))
+        .Returns(new ExpressionResult(Model.CancelAction));
+
+      // Act
+      var result = _sut.Handle(_element.Object, _model);
+
+      // Assert
+      Assert.NotNull(result, "Result nullability");
+      Assert.AreEqual(1, result.Elements.Length, "Count of results");
+      Assert.AreSame(_element.Object, result.Elements[0], "Correct element returned");
+      Mock.Get(_model).Verify(x => x.Evaluate(expression, _element.Object), Times.Once());
+      _element.Verify(x => x.SetAttribute(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()),
+                      Times.Never());
+      _element.Verify(x => x.SetAttribute(It.IsAny<string>(), It.IsAny<string>()),
+                      Times.Never());
+      _element.Verify(x => x.RemoveAttribute(It.IsAny<string>(), It.IsAny<string>()),
+                      Times.Never());
+      _element.Verify(x => x.RemoveAttribute(It.IsAny<string>()),
+                      Times.Never());
+    }
+
+    [Test]
+    [Description("This test verifies a multiple of different scenarios together")]
+    public void TestHandleMultipleAttributes()
+    {
+      // Arrange
+      var attributeValue = @"one expressionOne;
+                             two expressionTwo;
+                             three expression;;WithSemicolon;
+                             prefix:four expressionFour;";
+      _element
+        .Setup(x => x.GetAttribute(ZptConstants.Tal.Namespace,
+                                   ZptConstants.Tal.DefaultPrefix,
+                                   ZptConstants.Tal.AttributesAttribute))
+        .Returns(Mock.Of<ZptAttribute>(a => a.Value == attributeValue));
+
+      object
+        expressionOneResult = _autofixture.Create<string>(),
+        expressionTwoResult = null,
+        expressionThreeResult = Model.CancelAction,
+        expressionFourResult = _autofixture.Create<string>();
+
+      Mock.Get(_model)
+        .Setup(x => x.Evaluate("expressionOne", _element.Object))
+        .Returns(new ExpressionResult(expressionOneResult));
+      Mock.Get(_model)
+        .Setup(x => x.Evaluate("expressionTwo", _element.Object))
+        .Returns(new ExpressionResult(expressionTwoResult));
+      Mock.Get(_model)
+        .Setup(x => x.Evaluate("expression;WithSemicolon", _element.Object))
+        .Returns(new ExpressionResult(expressionThreeResult));
+      Mock.Get(_model)
+        .Setup(x => x.Evaluate("expressionFour", _element.Object))
+        .Returns(new ExpressionResult(expressionFourResult));
+
+      // Act
+      var result = _sut.Handle(_element.Object, _model);
+
+      // Assert
+      Assert.NotNull(result, "Result nullability");
+      Assert.AreEqual(1, result.Elements.Length, "Count of results");
+      Assert.AreSame(_element.Object, result.Elements[0], "Correct element returned");
+      _element.Verify(x => x.SetAttribute(null, "one", (string) expressionOneResult), Times.Once());
+      _element.Verify(x => x.RemoveAttribute(null, "two"), Times.Once());
+      _element.Verify(x => x.SetAttribute(null, "three", It.IsAny<string>()), Times.Never());
+      _element.Verify(x => x.SetAttribute("prefix", "four", (string) expressionFourResult), Times.Once());
+    }
+
+    #endregion
+  }
+}
+
