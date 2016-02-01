d84e8241a73429e02d616cbf4296f7f924ad7a87
Craig Fowler <craig@csf-dev.com>
2016-02-01 20:27:19 +0000

    Add customisation for creating TalesModel instances.

---
+++ Test.CSF.Zpt/Util/Autofixture/TalesModelCustomisation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Ploeh.AutoFixture;
+using CSF.Zpt.Tales;
+
+namespace Test.CSF.Zpt.Util.Autofixture
+{
+  public class TalesModelCustomisation : ICustomization
+  {
+    public void Customize(IFixture fixture)
+    {
+      fixture.Customize<TalesModel>(x => x.FromFactory((IEvaluatorRegistry reg) => new TalesModel(null, null, reg)));
+    }
+  }
+}
+
