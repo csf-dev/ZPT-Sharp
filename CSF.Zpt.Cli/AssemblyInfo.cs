83ffc2675c39fe39ab5348d704d93cf38183f450
Craig Fowler <craig@csf-dev.com>
2016-05-18 22:39:58 +0100

    Add empty projects for command-line app and MVC integration

---
+++ CSF.Zpt.Cli/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("CSF.Zpt.Cli")]
+[assembly: AssemblyDescription("Command-line utility for working with CSF.Zpt")]
