b4e317569fc5e15d1903e0e23e5c9e2093a625e7
Craig Fowler <craig@csf-dev.com>
2016-05-25 22:26:26 +0100

    WIP #22 - Create ZPT ViewEngine and View types
    These need improvement at some point when I decide what I'm doing with
    them.

---
+++ CSF.Zpt.MVC/ZptViewEngine.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace CSF.Zpt.MVC
+{
+  public class ZptViewEngine : VirtualPathProviderViewEngine
+  {
+    #region constants
+
+    private static readonly string[] ViewLocations = new [] {
+      "~/Views/{1}/{0}.pt",
+      "~/Views/Shared/{0}.pt",
+      "~/Views/{1}/{0}.xml",
+      "~/Views/Shared/{0}.xml",
+    };
+
+    #endregion
+
+    #region overrides
+
+    protected override IView CreateView(ControllerContext controllerContext, string viewPath, string masterPath)
+    {
+      return new ZptView(controllerContext.HttpContext.Server.MapPath(viewPath));
+    }
+
+    protected override IView CreatePartialView(ControllerContext controllerContext, string partialPath)
+    {
+      return new ZptView(controllerContext.HttpContext.Server.MapPath(partialPath));
+    }
+
+    #endregion
+
+    #region constructor
+
+    public ZptViewEngine()
+    {
+      this.ViewLocationFormats = ViewLocations;
+      this.PartialViewLocationFormats = ViewLocations;
+    }
+
+    #endregion
+  }
+}
+
