01e82c10e1fb5da4006863fdced1208e1a60d621
Craig Fowler <craig@csf-dev.com>
2016-08-29 13:12:38 +0100

    Trivial rename of API project to Abstractions

--- CSF.Zpt.Api/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-
-[assembly: AssemblyTitle("CSF.Zpt.Abstractions")]
-[assembly: AssemblyDescription("An implementation of Zope Page Templates for .NET - see " +
-                               "http://wiki.zope.org/ZPT/FrontPage")]
