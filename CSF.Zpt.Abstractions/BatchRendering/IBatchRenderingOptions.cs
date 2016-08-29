41e086d6861f15d43c0dbebd504c48fc728c2e79
Craig Fowler <craig@csf-dev.com>
2016-08-29 19:10:29 +0100

    WIP #78 - Add interfaces for batch rendering

---
+++ CSF.Zpt.Abstractions/BatchRendering/IBatchRenderingOptions.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CSF.Zpt.BatchRendering
+{
+  /// <summary>
+  /// Represents batch rendering options, indicating the inputs and outputs for a <see cref="IBatchRenderer"/>.
+  /// </summary>
+  public interface IBatchRenderingOptions
+  {
+    #region input options
+
+    /// <summary>
+    /// Gets the input stream, or a <c>null</c> reference if it is unset.
+    /// </summary>
+    /// <value>The input stream.</value>
+    Stream InputStream { get; }
+
+    /// <summary>
+    /// Gets the input paths, which may be an empty set if it is unset.
+    /// </summary>
+    /// <value>The input paths.</value>
+    IEnumerable<FileSystemInfo> InputPaths { get; }
+
+    /// <summary>
+    /// Gets a collection of ignored paths, which shall be excluded when examining the <see cref="InputPaths"/>.
+    /// </summary>
+    /// <value>The ignored paths.</value>
+    IEnumerable<DirectoryInfo> IgnoredPaths { get; }
+
+    /// <summary>
+    /// Gets a glob search pattern for examining the <see cref="InputPaths"/>.
+    /// </summary>
+    /// <value>The input search pattern.</value>
+    string InputSearchPattern { get; }
+
+    #endregion
+
+    #region output options
+
+    /// <summary>
+    /// Gets the output stream, or a <c>null</c> reference if it is unset.
+    /// </summary>
+    /// <value>The output stream.</value>
+    Stream OutputStream { get; }
+
+    /// <summary>
+    /// Gets the output path, or a <c>null</c> reference if it is unset.
+    /// </summary>
+    /// <value>The output path.</value>
+    FileSystemInfo OutputPath { get; }
+
+    /// <summary>
+    /// Gets an optional override for setting the file extension of written output files.
+    /// </summary>
+    /// <value>The output extension override.</value>
+    string OutputExtensionOverride { get; }
+
+    #endregion
+  }
+}
+
