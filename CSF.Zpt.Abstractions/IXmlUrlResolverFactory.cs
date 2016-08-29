5386c731d056a2b087b6c2ef5f09c376148b5140
Craig Fowler <craig@csf-dev.com>
2016-08-29 13:26:44 +0100

    WIP #81 - Add the concept of a factory type XmlUrlResolver instances

---
+++ CSF.Zpt.Abstractions/IXmlUrlResolverFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Xml;
+
+namespace CSF.Zpt
+{
+  public interface IXmlUrlResolverFactory
+  {
+    XmlUrlResolver GetResolver();
+  }
+}
+
---
+++ CSF.Zpt/XmlUrlResolverFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Xml;
+
+namespace CSF.Zpt
+{
+  public class XmlUrlResolverFactory : IXmlUrlResolverFactory
+  {
+    #region fields
+
+    private static readonly XmlUrlResolver _default;
+
+    #endregion
+
+    #region methods
+
+    public XmlUrlResolver GetResolver()
+    {
+      return _default;
+    }
+
+    #endregion
+
+    #region constructor
+
+    static XmlUrlResolverFactory()
+    {
+      _default = new LocalXhtmlXmlResolver();
+    }
+
+    #endregion
+  }
+}
+
