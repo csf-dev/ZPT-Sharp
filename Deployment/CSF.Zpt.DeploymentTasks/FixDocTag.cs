b09c5a071e31b50a48f84271ab85fe139df1f90e
Craig Fowler <craig@csf-dev.com>
2016-09-30 23:11:57 +0100

    WIP #89 - Reimplement the task to build the docs website

---
+++ Deployment/CSF.Zpt.DeploymentTasks/FixDocTag.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.Build.Utilities;
+using Microsoft.Build.Framework;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace CSF.Zpt.DeploymentTasks
+{
+  public class FixDocTag : Task
+  {
+    #region constants
+
+    private const string
+      OPEN_FORMAT   = @"<div metal:define-macro=""{0}"" tal:omit-tag=""string:True"">",
+      CLOSE_FORMAT  = @"</div>";
+
+    #endregion
+
+    #region properties
+
+    [Required]
+    public string InputFile { get; set; }
+
+    [Required]
+    public string MacroName { get; set; }
+
+    public string FirstMatch { get; set; }
+
+    public string SecondMatch { get; set; }
+
+    #endregion
+
+    #region methods
+
+    public override bool Execute()
+    {
+      var input = GetInputFile();
+
+      var output = PerformReplacements(input);
+
+      SaveOutput(output);
+
+      return true;
+    }
+
+    private string GetInputFile()
+    {
+      return File.ReadAllText(InputFile);
+    }
+
+    private string PerformReplacements(string fileContent)
+    {
+      var firstMatch = new Regex(FirstMatch);
+      var secondMatch = new Regex(SecondMatch);
+
+      var output = fileContent;
+
+      output = firstMatch.Replace(output, match => String.Format(OPEN_FORMAT, MacroName), 1);
+      output = secondMatch.Replace(output, match => String.Format(CLOSE_FORMAT, MacroName), 1);
+
+      return output;
+    }
+
+    private void SaveOutput(string output)
+    {
+      File.WriteAllText(InputFile, output);
+    }
+
+    #endregion
+
+    #region constructor
+
+    public FixDocTag()
+    {
+      FirstMatch  = "<hr>";
+      SecondMatch = "<hr>";
+    }
+
+    #endregion
+  }
+}
+
