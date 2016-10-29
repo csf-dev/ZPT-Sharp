e544ab65b810386a44c0e7b5f07883c9db72143d
Craig Fowler <craig@csf-dev.com>
2016-10-29 14:25:22 +0100

    Remove empty test project

--- Tests/Test.CSF.Zpt.DocumentProviders.XmlLinq/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-
-[assembly: AssemblyTitle("Test.CSF.Zpt.DocumentProviders.XmlLinq")]
-[assembly: AssemblyDescription("Tests for the CSF.Zpt.DocumentProviders.XmlLinq assembly")]
