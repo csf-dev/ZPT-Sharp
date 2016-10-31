4099dac56be3cb8212aeb5db2433deff74d99c84
Craig Fowler <craig@csf-dev.com>
2016-10-31 22:13:13 +0000

    WIP #114 - Unhook all remaining assemblies from CommonAssemblyInfo.cs

--- Tests/Test.CSF.Zpt.ExpressionEvaluators.NotExpressions/AssemblyInfo.cs
+++ Tests/Test.CSF.Zpt.ExpressionEvaluators.NotExpressions/AssemblyInfo.cs
@@ -1,5 +1,14 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
+[assembly: AssemblyCompany("CSF Software Limited")]
+[assembly: AssemblyProduct("CSF.Zpt - Zope Page Templates for .NET")]
+[assembly: AssemblyCopyright("CSF Software Limited")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyVersion("0.0.0")]
 [assembly: AssemblyTitle("Test.CSF.Zpt.ExpressionEvaluators.NotExpressions")]
 [assembly: AssemblyDescription("Tests for the CSF.Zpt.ExpressionEvaluators.NotExpressions assembly")]
