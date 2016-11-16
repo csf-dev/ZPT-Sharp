8b3d35058e8d6065b5cd3a62450a2a9428303e76
Craig Fowler <craig@csf-dev.com>
2016-11-16 21:49:09 +0000

    WIP #6 - Complete PoC succesfully

--- ExpressionEvaluators/CSF.Zpt.ExpressionEvaluators.PythonExpressions/PythonPoC.cs
+++ ExpressionEvaluators/CSF.Zpt.ExpressionEvaluators.PythonExpressions/PythonPoC.cs
@@ -5,9 +5,13 @@ using Microsoft.CSharp;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using IronPython.Runtime;
 
 namespace CSF.Zpt.ExpressionEvaluators.PythonExpressions
 {
+  /// <summary>
+  /// Proof of concept class for executing and evaluating Python expressions with an arbitrary set of variables.
+  /// </summary>
   public class PythonPoC
   {
     #region constants
@@ -16,73 +20,98 @@ namespace CSF.Zpt.ExpressionEvaluators.PythonExpressions
       CLASS_NAME = "PythonExpression",
       PYTHON_SCRIPT_TEMPLATE = @"
 class PythonExpression:
-{0}
-  def setVariable(self, name, val):
-{1}
-  def evaluate(self):
-    return {2}",
-    VARIABLE_DEF_TEMPLATE = "  {0} = None{1}",
-    VARIABLE_SET_TEMPLATE = @"    if name == '{0}':
-      {0} = val
-      return
-  {1}";
+  def evaluate(self, vals):
+    {0}
+    return {1}";
 
     #endregion
 
     #region fields
 
     private ScriptEngine _engine;
+    private ObjectOperations _operations;
 
     #endregion
 
     #region methods
 
+    /// <summary>
+    /// Evaluates the given expression, for the given collection of variable names/values.
+    /// </summary>
+    /// <param name="expression">The Python expression to evaluate.</param>
+    /// <param name="variableValues">The variable names &amp; values.</param>
     public object Evaluate(string expression, IDictionary<string,object> variableValues)
     {
       var scope = _engine.CreateScope();
-      var ops = _engine.Operations;
 
-      var script = CreateScript(expression, variableValues.Keys.ToArray());
+      var vals = SetupScript(expression, variableValues, scope);
+
+      var classDef = scope.GetVariable(CLASS_NAME);
+      var instance = _operations.CreateInstance(classDef);
+
+      return (object) instance.evaluate(vals);
+    }
+
+    /// <summary>
+    /// Sets up the script within a Python scope.
+    /// </summary>
+    /// <returns>A python tuple representing the (ordered) values of all of the variables.</returns>
+    /// <param name="expression">The expression to evaluate.</param>
+    /// <param name="variableValues">The variable names &amp; values.</param>
+    /// <param name="scope">The script scope.</param>
+    private PythonTuple SetupScript(string expression,
+                                    IDictionary<string,object> variableValues,
+                                    ScriptScope scope)
+    {
+      var allNamesAndValues = variableValues
+        .Select(x => new { Name = x.Key, Value = x.Value })
+        .OrderBy(x => x.Name)
+        .ToArray();
+
+      var script = CreateScript(expression, allNamesAndValues.Select(x => x.Name));
 
       _engine.Execute(script, scope);
 
-      var pythonClass = scope.GetVariable(CLASS_NAME);
-      var instance = ops.CreateInstance(pythonClass);
-
-      foreach(var pairing in variableValues)
-      {
-        instance.setVariable(pairing.Key, pairing.Value);
-      }
-
-      return (object) instance.evaluate();
+      return new PythonTuple(allNamesAndValues.Select(x => x.Value));
     }
 
+    /// <summary>
+    /// Creates the <c>System.String</c> python script contents.
+    /// </summary>
+    /// <returns>The script content.</returns>
+    /// <param name="expression">Expression.</param>
+    /// <param name="variableNames">Variable names.</param>
     private string CreateScript(string expression, IEnumerable<string> variableNames)
     {
-      StringBuilder
-        variableDefs = new StringBuilder(),
-        variableSetters = new StringBuilder();
+      string variables;
 
-      foreach(var name in variableNames)
+      if(variableNames.Count() == 0)
       {
-        variableDefs.AppendFormat(VARIABLE_DEF_TEMPLATE, name, Environment.NewLine);
-        variableSetters.AppendFormat(VARIABLE_SET_TEMPLATE, name, Environment.NewLine);
+        variables = String.Empty;
+      }
+      else if(variableNames.Count() == 1)
+      {
+        variables = String.Format("{0} = vals[0]");
+      }
+      else
+      {
+        variables = String.Format("{0} = vals", String.Join(", ", variableNames));
       }
 
-      var output = String.Format(PYTHON_SCRIPT_TEMPLATE, variableDefs, variableSetters, expression);
-
-      Console.WriteLine(output);
-
-      return output;
+      return String.Format(PYTHON_SCRIPT_TEMPLATE, variables, expression);
     }
 
     #endregion
 
     #region constructor
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CSF.Zpt.ExpressionEvaluators.PythonExpressions.PythonPoC"/> class.
+    /// </summary>
     public PythonPoC()
     {
       _engine = Python.CreateEngine();
+      _operations = _engine.Operations;
     }
 
     #endregion
