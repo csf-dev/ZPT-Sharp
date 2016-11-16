fdae2e5a3c5d84141e964ba0bc88e140131fea90
Craig Fowler <craig@csf-dev.com>
2016-11-16 20:32:05 +0000

    WIP #6 - Add working PoC for executing Python code from within the project

---
+++ ExpressionEvaluators/CSF.Zpt.ExpressionEvaluators.PythonExpressions/PythonPoC.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+using Microsoft.CSharp;
+
+namespace CSF.Zpt.ExpressionEvaluators.PythonExpressions
+{
+  public class PythonPoC
+  {
+    #region constants
+
+    private const string
+      CLASS_NAME = "PythonExpression",
+      PYTHON_SCRIPT = @"
+class PythonExpression:
+  def evaluate(self,a,b,c):
+    return a+b+c";
+
+    #endregion
+
+    #region fields
+
+    private ScriptEngine _engine;
+
+    #endregion
+
+    #region methods
+
+    public object Evaluate()
+    {
+      var scope = _engine.CreateScope();
+      var ops = _engine.Operations;
+
+      _engine.Execute(PYTHON_SCRIPT, scope);
+
+      var pythonClass = scope.GetVariable(CLASS_NAME);
+      var instance = ops.CreateInstance(pythonClass);
+
+      return (object) instance.evaluate(1, 2, 3);
+    }
+
+    #endregion
+
+    #region constructor
+
+    public PythonPoC()
+    {
+      _engine = Python.CreateEngine();
+    }
+
+    #endregion
+  }
+}
+
---
+++ Tests/Test.CSF.Zpt.ExpressionEvaluators.PythonExpressions/TestPythonPoC.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NUnit.Framework;
+using CSF.Zpt.ExpressionEvaluators.PythonExpressions;
+
+namespace Test.CSF.Zpt.ExpressionEvaluators.PythonExpressions
+{
+  [TestFixture]
+  public class TestPythonPoC
+  {
+    #region tests
+
+    [Test]
+    public void Evaluate_returns_expected_result()
+    {
+      // Arrange
+      var sut = new PythonPoC();
+
+      // Act
+      var result = sut.Evaluate();
+
+      // Assert
+      Assert.AreEqual(6, result);
+    }
+
+    #endregion
+  }
+}
+
