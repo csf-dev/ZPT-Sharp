9d93a55fbfaffae8ace4a4e09e92f60402d4735f
Craig Fowler <craig@csf-dev.com>
2017-02-02 20:12:00 +0000

    WIP #202 - Add 'load:' expression plugin assembly and integration tests

---
+++ ExpressionEvaluators/CSF.Zpt.ExpressionEvaluators.LoadExpressions/LoadExpressionEvaluator.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using CSF.Zpt.Tales;
+using CSF.Zpt.Rendering;
+
+namespace CSF.Zpt.ExpressionEvaluators.LoadExpressions
+{
+  /// <summary>
+  /// Implementation of <see cref="ExpressionEvaluatorBase"/> which evaluates a 'child' expression, converts the
+  /// result into a document which may be rendered, renders it and then returns the result as a string.
+  /// </summary>
+  public class LoadExpressionEvaluator : ExpressionEvaluatorBase
+  {
+    #region constants
+
+    private static readonly string Prefix = "load";
+
+    #endregion
+
+    #region properties
+
+    /// <summary>
+    /// Gets the expression prefix handled by the current evaluator instance.
+    /// </summary>
+    /// <value>The prefix.</value>
+    public override string ExpressionPrefix
+    {
+      get {
+        return Prefix;
+      }
+    }
+
+    #endregion
+
+    #region methods
+
+    /// <summary>
+    /// Evaluate the specified expression, for the given element and model.
+    /// </summary>
+    /// <param name="expression">The expression to evaluate.</param>
+    /// <param name="context">The rendering context for the expression being evaluated.</param>
+    /// <param name="model">The ZPT model, providing the context for evaluation.</param>
+    public override ExpressionResult Evaluate(Expression expression, IRenderingContext context, ITalesModel model)
+    {
+      if(expression == null)
+      {
+        throw new ArgumentNullException(nameof(expression));
+      }
+      if(context == null)
+      {
+        throw new ArgumentNullException(nameof(context));
+      }
+      if(model == null)
+      {
+        throw new ArgumentNullException(nameof(model));
+      }
+
+      var innerExpressionResult = GetExpressionResult(expression, context, model);
+
+      if(innerExpressionResult.CancelsAction
+         || ReferenceEquals(innerExpressionResult.Value, null))
+      {
+        return innerExpressionResult;
+      }
+
+      return new ExpressionResult(RenderDocument(innerExpressionResult.Value, context));
+    }
+
+    /// <summary>
+    /// Gets the result of the given 'inner' expression.
+    /// </summary>
+    /// <returns>The expression result.</returns>
+    /// <param name="expression">Expression.</param>
+    /// <param name="context">Context.</param>
+    /// <param name="model">Model.</param>
+    protected virtual ExpressionResult GetExpressionResult(Expression expression,
+                                                           IRenderingContext context,
+                                                           ITalesModel model)
+    {
+      if(expression == null)
+      {
+        throw new ArgumentNullException(nameof(expression));
+      }
+      if(context == null)
+      {
+        throw new ArgumentNullException(nameof(context));
+      }
+      if(model == null)
+      {
+        throw new ArgumentNullException(nameof(model));
+      }
+
+      var trimmedContent = expression.Content.TrimStart();
+      return model.Evaluate(ExpressionCreator.Create(trimmedContent), context);
+    }
+
+    /// <summary>
+    /// Renders a document and returns its result.
+    /// </summary>
+    /// <returns>The document.</returns>
+    /// <param name="document">Document.</param>
+    /// <param name="context">Context.</param>
+    protected string RenderDocument(object document, IRenderingContext context)
+    {
+      if(document == null)
+      {
+        throw new ArgumentNullException(nameof(document));
+      }
+      if(context == null)
+      {
+        throw new ArgumentNullException(nameof(context));
+      }
+
+      // TODO: Write this implementation
+      throw new NotImplementedException();
+    }
+
+    #endregion
+  }
+}
+
---
+++ Tests/Test.CSF.Zpt.Integration/LoadPluginTests.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using NUnit.Framework;
+using System.IO;
+using CSF.Zpt.Tales;
+using System.Collections.Generic;
+using CSF.Zpt;
+using CSF.Zpt.Rendering;
+using System.Linq;
+
+namespace Test.CSF.Zpt.Integration
+{
+  [TestFixture]
+  public class LoadPluginTests : IntegrationTestBase
+  {
+    #region fields
+
+    private ModelClass _model;
+
+    #endregion
+
+    #region tests
+
+    [Test]
+    public void RunIntegrationTests()
+    {
+      // Arrange
+      var filePairsToTest = this.GetFilePairsToTest();
+      var failedTests = new List<FileInfo>();
+
+      // Act
+      foreach(var pair in filePairsToTest)
+      {
+        if(!PerformTestRun(pair.Item1, pair.Item2))
+        {
+          failedTests.Add(pair.Item2);
+        }
+      }
+
+      this.Logger.InfoFormat("{0} integration test cases processed", filePairsToTest.Count());
+
+      // Assert
+      Assert.That(!failedTests.Any(),
+                  "Out of {0} integration tests, {1} failed. See the log file for more info.",
+                  filePairsToTest.Count(),
+                  failedTests.Count());
+    }
+
+    #endregion
+
+    #region methods
+
+    protected override DirectoryInfo GetSourcePath(IIntegrationTestConfiguration config)
+    {
+      return config.GetSourceDocumentPath(IntegrationTestType.LoadPlugin);
+    }
+
+    protected override DirectoryInfo GetExpectedPath(IIntegrationTestConfiguration config)
+    {
+      return config.GetExpectedOutputPath(IntegrationTestType.LoadPlugin);
+    }
+
+    protected override void PerformExtraSetup()
+    {
+      _model = new ModelClass();
+
+      base.PerformExtraSetup();
+    }
+
+    protected override string Render(IZptDocument document, IRenderingSettings options)
+    {
+      return document.Render(_model, options);
+    }
+
+    #endregion
+
+    #region test class
+
+    public class ModelClass
+    {
+      public TemplateDirectory Documents { get; private set; }
+
+      public string Name { get; private set; }
+
+      public IEnumerable<SampleClass> Items { get; private set; }
+
+      public ModelClass(DirectoryInfo sourceDir, string name)
+      {
+        Documents = new TemplateDirectory(sourceDir);
+        Name = name;
+        Items = new LinkedList<SampleClass>() {
+          new SampleClass() { Name = "One" },
+          new SampleClass() { Name = "Two" },
+          new SampleClass() { Name = "Three" },
+        };
+      }
+    }
+
+    public class SampleClass
+    {
+      public string Name { get; set; }
+    }
+
+    #endregion
+  }
+}
+
