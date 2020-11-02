0ba44d0e0943b5e6db835f07ee7ffd0a06feb4f4
Craig Fowler <craig@csf-dev.com>
2020-11-02 14:08:15 +0000

    WIP #258 - Add test coverage

---
+++ ZptSharp.Tests/Dom/DocumentReaderWriterBaseTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using ZptSharp.Autofixture;
+using ZptSharp.Config;
+using ZptSharp.Rendering;
+
+namespace ZptSharp.Dom
+{
+    [TestFixture,Parallelizable]
+    public class DocumentReaderWriterBaseTests
+    {
+        [Test, AutoMoqData]
+        public void GetDocumentAsync_throws_ANE_if_stream_is_null(StubDocumentReaderWriter sut,
+                                                                  [MockedConfig] RenderingConfig config)
+        {
+            Assert.That(() => sut.GetDocumentAsync(null, config), Throws.ArgumentNullException);
+        }
+
+        [Test, AutoMoqData]
+        public void GetDocumentAsync_throws_ANE_if_config_is_null(StubDocumentReaderWriter sut,
+                                                                  Stream stream)
+        {
+            Assert.That(() => sut.GetDocumentAsync(stream, null), Throws.ArgumentNullException);
+        }
+
+        [Test, AutoMoqData]
+        public void GetDocumentAsync_throws_if_operation_is_cancelled(StubDocumentReaderWriter sut,
+                                                                      Stream stream,
+                                                                      [MockedConfig] RenderingConfig config)
+        {
+            var tokenSource = new CancellationTokenSource();
+            tokenSource.Cancel();
+
+            Assert.That(() => sut.GetDocumentAsync(stream, config, token: tokenSource.Token), Throws.InstanceOf<OperationCanceledException>());
+        }
+
+        [Test, AutoMoqData]
+        public void WriteDocumentAsync_throws_ANE_if_doc_is_null(StubDocumentReaderWriter sut,
+                                                                [MockedConfig] RenderingConfig config)
+        {
+            Assert.That(() => sut.WriteDocumentAsync(null, config), Throws.ArgumentNullException);
+        }
+
+        [Test, AutoMoqData]
+        public void WriteDocumentAsync_throws_ArgEx_if_doc_is_wrong_type(StubDocumentReaderWriter sut,
+                                                                         [MockedConfig] RenderingConfig config,
+                                                                         IDocument doc)
+        {
+            Assert.That(() => sut.WriteDocumentAsync(doc, config), Throws.ArgumentException);
+        }
+
+        [Test, AutoMoqData]
+        public void WriteDocumentAsync_throws_ANE_if_config_is_null(StubDocumentReaderWriter sut,
+                                                                    StubDocument doc)
+        {
+            Assert.That(() => sut.WriteDocumentAsync(doc, null), Throws.ArgumentNullException);
+        }
+
+        [Test, AutoMoqData]
+        public void WriteDocumentAsync_throws_if_operation_is_cancelled(StubDocumentReaderWriter sut,
+                                                                        StubDocument doc,
+                                                                        [MockedConfig] RenderingConfig config)
+        {
+            var tokenSource = new CancellationTokenSource();
+            tokenSource.Cancel();
+
+            Assert.That(() => sut.WriteDocumentAsync(doc, config, token: tokenSource.Token), Throws.InstanceOf<OperationCanceledException>());
+        }
+
+        /// <summary>
+        /// A stub reader/writer with hard-coded logic, for the purpose of testing the base class.
+        /// </summary>
+        public class StubDocumentReaderWriter : DocumentReaderWriterBase<StubDocument>
+        {
+            public override bool CanReadWriteForFilename(string filenameOrPath) => true;
+
+            protected override Task<IDocument> GetDocumentProtectedAsync(Stream stream, RenderingConfig config, IDocumentSourceInfo sourceInfo, CancellationToken token)
+                => Task.FromResult(Mock.Of<IDocument>());
+
+            protected override Task<Stream> WriteDocumentProtectedAsync(StubDocument document, RenderingConfig config, CancellationToken token)
+                => Task.FromResult<Stream>(new MemoryStream());
+        }
+    }
+}
---
+++ ZptSharp.Tests/Expressions/ExpressionEvaluatorExtensionsTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+
+namespace ZptSharp.Expressions
+{
+    [TestFixture,Parallelizable]
+    public class ExpressionEvaluatorExtensionsTests
+    {
+        [Test, AutoMoqData]
+        public void EvaluateExpressionAsync_throws_ANE_if_evaluator_is_null(string expression,
+                                                                            ExpressionContext context)
+        {
+            IEvaluatesExpression evaluator = null;
+            Assert.That(() => evaluator.EvaluateExpressionAsync<string>(expression, context), Throws.ArgumentNullException);
+        }
+
+        [Test, AutoMoqData]
+        public async Task EvaluateExpressionAsync_uses_evaluator_to_resolve_expression(IEvaluatesExpression evaluator,
+                                                                                       string expression,
+                                                                                       ExpressionContext context,
+                                                                                       string expected)
+        {
+            Mock.Get(evaluator)
+                .Setup(x => x.EvaluateExpressionAsync(expression, context, CancellationToken.None))
+                .Returns(() => Task.FromResult<object>(expected));
+
+            var result = await evaluator.EvaluateExpressionAsync<string>(expression, context);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test, AutoMoqData]
+        public void EvaluateExpressionAsync_throws_EvaluationException_if_result_is_wrong_type(IEvaluatesExpression evaluator,
+                                                                                               string expression,
+                                                                                               ExpressionContext context,
+                                                                                               int result)
+        {
+            Mock.Get(evaluator)
+                .Setup(x => x.EvaluateExpressionAsync(expression, context, CancellationToken.None))
+                .Returns(() => Task.FromResult<object>(result));
+
+            Assert.That(() => evaluator.EvaluateExpressionAsync<string>(expression, context).Result,
+                        Throws.InstanceOf<AggregateException>().And.InnerException.InstanceOf<EvaluationException>());
+        }
+    }
+}
