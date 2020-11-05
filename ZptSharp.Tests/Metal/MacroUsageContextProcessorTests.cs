a696b2aa2cd2a97121149c7f5d9a681512e8b8b6
Craig Fowler <craig@csf-dev.com>
2020-11-05 17:39:35 +0000

    WIP #258 - Fix some tests

--- ZptSharp.Tests/Metal/MacroUsageContextProcessorTests.cs
+++ ZptSharp.Tests/Metal/MacroUsageContextProcessorTests.cs
@@ -16,13 +16,16 @@ namespace ZptSharp.Metal
     {
         [Test, AutoMoqData]
         public async Task ProcessContextAsync_does_not_replace_element_if_no_use_macro_attribute([Frozen] IGetsMetalAttributeSpecs specProvider,
+                                                                                                 [Frozen] IGetsMacro macroProvider,
                                                                                                  MacroUsageContextProcessor sut,
                                                                                                  AttributeSpec spec,
                                                                                                  ExpressionContext context,
                                                                                                  IElement element)
         {
             Mock.Get(specProvider).SetupGet(x => x.UseMacro).Returns(spec);
-            Mock.Get(element).SetupGet(x => x.Attributes).Returns(new List<IAttribute>());
+            Mock.Get(macroProvider)
+                .Setup(x => x.GetMacroAsync(element, context, spec, CancellationToken.None))
+                .Returns(Task.FromResult<MetalMacro>(null));
             context.CurrentElement = element;
 
             await sut.ProcessContextAsync(context);
@@ -32,7 +35,7 @@ namespace ZptSharp.Metal
 
         [Test, AutoMoqData]
         public async Task ProcessContextAsync_replaces_element_in_context_with_expanded_macro([Frozen] IGetsMetalAttributeSpecs specProvider,
-                                                                                              [Frozen] IEvaluatesExpression expressionEvaluator,
+                                                                                              [Frozen] IGetsMacro macroProvider,
                                                                                               [Frozen] IExpandsMacro macroExpander,
                                                                                               MacroUsageContextProcessor sut,
                                                                                               MetalMacro macro,
@@ -48,9 +51,9 @@ namespace ZptSharp.Metal
             Mock.Get(attribute).Setup(x => x.Matches(spec)).Returns(true);
             Mock.Get(attribute).SetupGet(x => x.Value).Returns(expression);
             context.CurrentElement = element;
-            Mock.Get(expressionEvaluator)
-                .Setup(x => x.EvaluateExpressionAsync(expression, context, CancellationToken.None))
-                .Returns(() => Task.FromResult<object>(macro));
+            Mock.Get(macroProvider)
+                .Setup(x => x.GetMacroAsync(element, context, spec, CancellationToken.None))
+                .Returns(Task.FromResult(macro));
             Mock.Get(macroExpander)
                 .Setup(x => x.ExpandMacroAsync(macro, context, CancellationToken.None))
                 .Returns(() => Task.FromResult(expandedMacro));
@@ -58,29 +61,6 @@ namespace ZptSharp.Metal
             await sut.ProcessContextAsync(context);
 
             Assert.That(context.CurrentElement, Is.SameAs(expandedMacro.Element));
-        }
-
-        [Test, AutoMoqData]
-        public void ProcessContextAsync_throws_MacroNotFoundException_if_macro_not_found([Frozen] IGetsMetalAttributeSpecs specProvider,
-                                                                                         [Frozen] IEvaluatesExpression expressionEvaluator,
-                                                                                         MacroUsageContextProcessor sut,
-                                                                                         AttributeSpec spec,
-                                                                                         ExpressionContext context,
-                                                                                         IElement element,
-                                                                                         IAttribute attribute,
-                                                                                         string expression)
-        {
-            Mock.Get(specProvider).SetupGet(x => x.UseMacro).Returns(spec);
-            Mock.Get(element).SetupGet(x => x.Attributes).Returns(new List<IAttribute> { attribute });
-            Mock.Get(attribute).Setup(x => x.Matches(spec)).Returns(true);
-            Mock.Get(attribute).SetupGet(x => x.Value).Returns(expression);
-            context.CurrentElement = element;
-            Mock.Get(expressionEvaluator)
-                .Setup(x => x.EvaluateExpressionAsync(expression, context, CancellationToken.None))
-                .Returns(() => Task.FromResult<object>(null));
-
-            Assert.That(() => sut.ProcessContextAsync(context).Result,
-                        Throws.InstanceOf<AggregateException>().And.InnerException.InstanceOf<MacroNotFoundException>());
         }
     }
 }
