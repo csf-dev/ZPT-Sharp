3004b322869877545a639990f7c07bb416b668a6
Craig Fowler <craig@csf-dev.com>
2020-11-06 19:14:34 +0000

    WIP #258 - Add impl for registry-based evaluator

---
+++ ZptSharp.Impl/Expressions/RegistryBasedExpressionEvaluator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ZptSharp.Expressions
+{
+    /// <summary>
+    /// <para>
+    /// An implementation of <see cref="IEvaluatesExpression"/> which makes use of
+    /// the registry pattern to select and delegate execution to another implementation (of
+    /// the same interface).  This selection occurs based upon the expression type.
+    /// </para>
+    /// <para>
+    /// Additionally, when delegating to an implementation of <see cref="IEvaluatesExpression"/>
+    /// from the registry, any expression-type prefix upon the expression is removed before
+    /// this delegation occurs.
+    /// </para>
+    /// </summary>
+    public class RegistryBasedExpressionEvaluator : IEvaluatesExpression
+    {
+        readonly IGetsExpressionType typeProvider;
+        readonly IGetsEvaluatorForExpressionType evaluatorProvider;
+        readonly IRemovesPrefixFromExpression prefixRemover;
+
+        /// <summary>
+        /// Evaluates the expression asynchronously and returns the result.
+        /// </summary>
+        /// <returns>The expression result.</returns>
+        /// <param name="expression">The expression string.</param>
+        /// <param name="context">The expression context.</param>
+        /// <param name="cancellationToken">An optional cancellation token.</param>
+        public Task<object> EvaluateExpressionAsync(string expression,
+                                                    ExpressionContext context,
+                                                    CancellationToken cancellationToken = default)
+        {
+            var expressionType = typeProvider.GetExpressionType(expression);
+            var evaluator = evaluatorProvider.GetEvaluator(expressionType);
+            var expressionWithoutPrefix = prefixRemover.GetExpressionWithoutPrefix(expression);
+
+            return evaluator.EvaluateExpressionAsync(expressionWithoutPrefix, context, cancellationToken);
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RegistryBasedExpressionEvaluator"/> class.
+        /// </summary>
+        /// <param name="typeProvider">Type provider.</param>
+        /// <param name="evaluatorProvider">Evaluator provider.</param>
+        /// <param name="prefixRemover">Prefix remover.</param>
+        public RegistryBasedExpressionEvaluator(IGetsExpressionType typeProvider,
+                                                IGetsEvaluatorForExpressionType evaluatorProvider,
+                                                IRemovesPrefixFromExpression prefixRemover)
+        {
+            this.typeProvider = typeProvider ?? throw new ArgumentNullException(nameof(typeProvider));
+            this.evaluatorProvider = evaluatorProvider ?? throw new ArgumentNullException(nameof(evaluatorProvider));
+            this.prefixRemover = prefixRemover ?? throw new ArgumentNullException(nameof(prefixRemover));
+        }
+    }
+}
---
+++ ZptSharp.Tests/Expressions/RegistryBasedExpressionEvaluatorTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture.NUnit3;
+using Moq;
+using NUnit.Framework;
+
+namespace ZptSharp.Expressions
+{
+    [TestFixture,Parallelizable]
+    public class RegistryBasedExpressionEvaluatorTests
+    {
+        [Test, AutoMoqData]
+        public void EvaluateExpressionAsync_returns_result_from_resolved_evaluator([Frozen] IGetsExpressionType typeProvider,
+                                                                                   [Frozen] IGetsEvaluatorForExpressionType evaluatorProvider,
+                                                                                   [Frozen] IRemovesPrefixFromExpression prefixRemover,
+                                                                                   RegistryBasedExpressionEvaluator sut,
+                                                                                   string expression,
+                                                                                   string type,
+                                                                                   string expressionNoPrefix,
+                                                                                   IEvaluatesExpression evaluator,
+                                                                                   object expected,
+                                                                                   ExpressionContext context)
+        {
+            Mock.Get(typeProvider)
+                .Setup(x => x.GetExpressionType(expression))
+                .Returns(type);
+            Mock.Get(evaluatorProvider)
+                .Setup(x => x.GetEvaluator(type))
+                .Returns(evaluator);
+            Mock.Get(prefixRemover)
+                .Setup(x => x.GetExpressionWithoutPrefix(expression))
+                .Returns(expressionNoPrefix);
+            Mock.Get(evaluator)
+                .Setup(x => x.EvaluateExpressionAsync(expressionNoPrefix, context, CancellationToken.None))
+                .Returns(() => Task.FromResult(expected));
+
+            Assert.That(() => sut.EvaluateExpressionAsync(expression, context, CancellationToken.None).Result, Is.SameAs(expected));
+        }
+    }
+}
