7a260fb2f6b45129e2c9f963927c665c9a8de40d
Craig Fowler <craig@csf-dev.com>
2020-11-16 19:37:56 +0000

    WIP #258 - Add test coverage

---
+++ ZptSharp.Tests/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+
+namespace ZptSharp
+{
+    [TestFixture,Parallelizable]
+    public class ServiceCollectionExtensionsTests
+    {
+        [Test]
+        public void AddZptSharp_does_not_throw()
+        {
+            var collection = new ServiceCollection();
+            Assert.That(() => collection.AddZptSharp(), Throws.Nothing);
+        }
+
+        [Test]
+        public void UseZptPathExpressions_does_not_throw()
+        {
+            var collection = new ServiceCollection();
+            collection.AddZptSharp();
+            var provider = collection.BuildServiceProvider();
+
+            Assert.That(() => provider.UseZptPathExpressions(), Throws.Nothing);
+        }
+    }
+}
