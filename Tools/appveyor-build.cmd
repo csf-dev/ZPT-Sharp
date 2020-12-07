950d0825da5ba2f4aa1b8bd9e5a651bb779dc2f6
Craig Fowler <craig@csf-dev.com>
2020-12-07 19:26:09 +0000

    Another attempt to improve the build, using a cmd script
    PowerShell didn't work but maybe this will

---
+++ Tools/appveyor-build.cmd
@@ -0,0 +1,27 @@
+﻿dotnet --version
+
+dotnet-sonarscanner begin ^
+    /k:"ZptSharp" ^
+    /v:AppVeyor_build_%APPVEYOR_BUILD_NUMBER% ^
+    /o:craigfowler-github ^
+    /s:%APPVEYOR_BUILD_FOLDER%\.SonarQube.Analysis.xml ^
+    /d:sonar.host.url="https://sonarcloud.io" ^
+    /d:sonar.login=%SONARCLOUD_SECRET_KEY% ^
+    /d:sonar.cs.nunit.reportsPaths=%APPVEYOR_BUILD_FOLDER%\.TestResults\TestResults.xml ^
+    /d:sonar.cs.opencover.reportsPaths=%APPVEYOR_BUILD_FOLDER%\.TestResults\coverage.opencover.xml
+    
+dotnet test ^
+    /p:CollectCoverage=true ^
+    /p:CoverletOutputFormat=\"json,opencover\" ^
+    /p:CoverletOutput=\"../.TestResults/\" ^
+    --test-adapter-path:. ^
+    --logger:\"nunit;LogFilePath=../.TestResults\TestResults.xml\"
+
+SET exitcode=%errorlevel%
+
+dotnet-sonarscanner end ^
+    /d:"sonar.login=%SONARCLOUD_SECRET_KEY%"
+
+for %%file in (.TestResults\**\*) do appveyor PushArtifact %%file
+
+exit /B %exitcode%
