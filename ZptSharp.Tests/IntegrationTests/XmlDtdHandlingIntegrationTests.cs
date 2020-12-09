cfcf1474c2c77ff41a2d5276975dbab3820ccfcf
Craig Fowler <craig@csf-dev.com>
2020-12-09 17:41:04 +0000

    Resolve #261 - Apparently there is no need to add further logic?
    This integration test shows that the XML document provider
    can already handle documents with DTDs just fine.  They load,
    parse and can emit renderings.
    
    It appears that no further change is required.

---
+++ ZptSharp.Tests/IntegrationTests/XmlDtdHandlingIntegrationTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ZptSharp.Util;
+
+namespace ZptSharp.IntegrationTests
+{
+    [TestFixture, Parallelizable, Category("Integration")]
+    public class XmlDtdHandlingIntegrationTests
+    {
+        // The purpose of these integration tests is to show that ZptSharp can read a document which uses DTDs.
+
+        [Test, Description("For the 'expected rendering' file, rendering the corresponding 'source document' file via ZptSharp should produce the same output.")]
+        public async Task Each_output_file_should_render_as_expected([ValueSource(nameof(GetExpectedOutputFiles))] string expectedPath)
+        {
+            var result = await IntegrationTester.PerformIntegrationTest(expectedPath);
+            Assert.That(result, Has.MatchingExpectedAndActualRenderings);
+        }
+
+        public static IEnumerable<string> GetExpectedOutputFiles() => TestFiles.GetIntegrationTestExpectedFiles<XmlDtdHandlingIntegrationTests>();
+    }
+}
