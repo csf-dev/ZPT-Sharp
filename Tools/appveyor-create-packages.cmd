58219cddaae02a375bdba8601556fd301aba955d
Craig Fowler <craig@csf-dev.com>
2020-12-11 18:55:00 +0000

    WIP #278 - Attempt to fix package script

--- Tools/appveyor-create-packages.cmd
+++ Tools/appveyor-create-packages.cmd
@@ -1,10 +1,13 @@
+IF %APPVEYOR_REPO_TAG%==true (
+    SET package_version=%APPVEYOR_REPO_TAG_NAME:~1%
+) ELSE (
+    SET package_version=%APPVEYOR_BUILD_VERSION%
+)
+
 REM ---
 REM Create the packages
 REM ---
-IF "%APPVEYOR_REPO_TAG%"=="true"
-    dotnet pack -c Release -o .Packages -p:PackageVersion=%APPVEYOR_REPO_TAG_NAME:~1%
-ELSE
-    dotnet pack -c Release -o .Packages 
+dotnet pack -c Release -o .Packages -p:PackageVersion=%package_version%
 
 REM ---
 REM Upload those packages into AppVeyor as artifacts
@@ -14,5 +17,6 @@ FOR %%F IN (.Packages\*.nupkg) DO appveyor PushArtifact %%F
 REM ---
 REM If we built from a tag then push those packages
 REM ---
-IF "%APPVEYOR_REPO_TAG%"=="true"
+IF "%APPVEYOR_REPO_TAG%"=="true" (
     FOR %%F IN (.Packages\*.nupkg) DO dotnet push %%F --api-key %NUGET_SECRET_API_KEY%
+)
