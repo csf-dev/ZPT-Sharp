0a01e1caaf55c1789a4651daee034695940f6758
Craig Fowler <craig@csf-dev.com>
2020-12-11 19:00:55 +0000

    WIP #278 - Further packaging script improvements

--- Tools/appveyor-create-packages.cmd
+++ Tools/appveyor-create-packages.cmd
@@ -1,13 +1,13 @@
-IF %APPVEYOR_REPO_TAG%==true (
-    SET package_version=%APPVEYOR_REPO_TAG_NAME:~1%
-) ELSE (
-    SET package_version=%APPVEYOR_BUILD_VERSION%
-)
-
 REM ---
 REM Create the packages
 REM ---
-dotnet pack -c Release -o .Packages -p:PackageVersion=%package_version%
+IF %APPVEYOR_REPO_TAG%==true (
+    SET package_version=%APPVEYOR_REPO_TAG_NAME:~1%
+    dotnet pack -c Release -o .Packages -p:PackageVersion=%package_version%
+) ELSE (
+    SET version_suffix=-ci-build.%APPVEYOR_BUILD_NUMBER%
+    dotnet pack -c Release -o .Packages --version-suffix %version_suffix%
+)
 
 REM ---
 REM Upload those packages into AppVeyor as artifacts
@@ -17,6 +17,7 @@ FOR %%F IN (.Packages\*.nupkg) DO appveyor PushArtifact %%F
 REM ---
 REM If we built from a tag then push those packages
 REM ---
-IF "%APPVEYOR_REPO_TAG%"=="true" (
-    FOR %%F IN (.Packages\*.nupkg) DO dotnet push %%F --api-key %NUGET_SECRET_API_KEY%
+IF %APPVEYOR_REPO_TAG%==true (
+    FOR %%F IN (.Packages\*.nupkg) DO ECHO dotnet push %%F --api-key KEY!
+    REM FOR %%F IN (.Packages\*.nupkg) DO dotnet push %%F --api-key %NUGET_SECRET_API_KEY%
 )
