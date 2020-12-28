6d1c1ed1e670f2853ecf860a8ee99c302965d7a2
Craig Fowler <craig@csf-dev.com>
2020-12-28 17:50:46 +0000

    WIP #262 - Work to make sample project runnable

--- Examples/ZptSharp.Examples.Mvc5/Controllers/HomeController.cs
+++ Examples/ZptSharp.Examples.Mvc5/Controllers/HomeController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Mvc.Ajax;
+﻿using System.Web.Mvc;
 
 namespace ZptSharp.Examples.Controllers
 {
@@ -11,13 +6,7 @@ namespace ZptSharp.Examples.Controllers
     {
         public ActionResult Index()
         {
-            var mvcName = typeof(Controller).Assembly.GetName();
-            var isMono = Type.GetType("Mono.Runtime") != null;
-
-            ViewData["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
-            ViewData["Runtime"] = isMono ? "Mono" : ".NET";
-
-            return View();
+            return View(new { Message = "Hello world!" });
         }
     }
 }
--- Examples/ZptSharp.Examples.Mvc5/Global.asax.cs
+++ Examples/ZptSharp.Examples.Mvc5/Global.asax.cs
@@ -2,6 +2,7 @@
 using System.Web.Mvc;
 using System.Web.Routing;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace ZptSharp.Examples
 {
@@ -12,7 +13,26 @@ namespace ZptSharp.Examples
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
 
+            var zptSharpViewEngine = GetZptSharpViewEngine();
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(zptSharpViewEngine);
+        }
+
+        IViewEngine GetZptSharpViewEngine()
+        {
             var services = new ServiceCollection();
+            services
+                .AddZptSharp()
+                .AddHapZptDocuments()
+                .AddZptSharpMvc5ViewEngine()
+                .AddLogging();
+
+            var provider = services.BuildServiceProvider();
+            provider
+                .UseHapZptDocuments()
+                .UseStandardZptExpressions();
+
+            return provider.GetZptSharpMvc5ViewEngine();
         }
     }
 }
