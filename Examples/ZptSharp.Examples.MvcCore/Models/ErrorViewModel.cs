923274a026fb3ede3152e014190cd2371d8832d0
Craig Fowler <craig@csf-dev.com>
2020-12-31 11:33:56 +0000

    WIP #294 - Initial commit for MVC Core sample app
    This is no more than a blank dotnet 3.1 MVC template web app.

---
+++ Examples/ZptSharp.Examples.MvcCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ZptSharp.Examples.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
