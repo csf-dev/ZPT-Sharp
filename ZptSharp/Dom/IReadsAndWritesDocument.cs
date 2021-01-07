f2a46d5bc7ede2d2ff43a9b719c2a2f152e61d8e
Craig Fowler <craig@craigfowler.me.uk>
2021-01-07 13:45:41 +0000

    WIP #296 - Add further docs

--- ZptSharp/Dom/IReadsAndWritesDocument.cs
+++ ZptSharp/Dom/IReadsAndWritesDocument.cs
@@ -8,16 +8,57 @@ namespace ZptSharp.Dom
 {
     /// <summary>
     /// An object which may read &amp; write <see cref="IDocument"/> instances to/from
-    /// streams.
+    /// streams.  This is the core interface of "a document provider".
     /// </summary>
+    /// <remarks>
+    /// <para>
+    /// A document provider is a required add-on for ZptSharp which allows it to read and write
+    /// markup documents.
+    /// On its own, the ZptSharp core deals only in an abstraction, the <see cref="IDocument"/> interface
+    /// and its members.
+    /// The document provider is how a stream may be read as a document and how a modified/rendered
+    /// document may be written/saved back to a stream.
+    /// </para>
+    /// <para>
+    /// Implementations of this interface typically use other/3rd-party libraries to read/write the
+    /// markup, and "bridge the gap" between the specific document reading/writing API, and the abstractions
+    /// which the ZptSharp core uses.
+    /// </para>
+    /// <para>
+    /// In order to have a working/usable ZptSharp environment, at least one document provider
+    /// must be registered with dependency injection and must also be registered for use with
+    /// <see cref="IRegistersDocumentReaderWriter"/>.
+    /// This registration is usually accomplished via extension methods for
+    /// <see cref="Microsoft.Extensions.DependencyInjection.IServiceCollection"/> and
+    /// <see cref="System.IServiceProvider"/> and does not usually need to be done manually.
+    /// </para>
+    /// </remarks>
+    /// <seealso cref="IDocument"/>
+    /// <seealso cref="INode"/>
+    /// <seealso cref="IAttribute"/>
+    /// <seealso cref="IRegistersDocumentReaderWriter"/>
     public interface IReadsAndWritesDocument
     {
         /// <summary>
-        /// Gets whether or not the current instance may be used to read &amp; write documents
+        /// Gets a value which indicates whether or not the current instance may be used to read &amp; write documents
         /// which have the specified filename.
         /// </summary>
-        /// <returns><c>true</c>, if this instance maybe used, <c>false</c> otherwise.</returns>
-        /// <param name="filenameOrPath">The filename of a ZPT document.</param>
+        /// <remarks>
+        /// <para>
+        /// This is the mechanism by which an <see cref="IGetsZptDocumentRendererForFilePath"/>
+        /// (and ultimately an <see cref="IRendersZptFile"/>) selects an appropriate document provider implementation
+        /// for a specified file.
+        /// </para>
+        /// <para>
+        /// This enables document providers to be used with the strategy pattern -
+        /// <see href="https://en.wikipedia.org/wiki/Strategy_pattern" /> - interrogating each of the available providers
+        /// to determine which can perform the required task.
+        /// </para>
+        /// </remarks>
+        /// <returns><c>true</c>, if this instance may be used for the specified file name/file path; <c>false</c> if not.</returns>
+        /// <param name="filenameOrPath">The file name or file path of a ZPT document.</param>
+        /// <seealso cref="IGetsZptDocumentRendererForFilePath"/>
+        /// <seealso cref="IRendersZptFile"/>
         bool CanReadWriteForFilename(string filenameOrPath);
 
         /// <summary>
