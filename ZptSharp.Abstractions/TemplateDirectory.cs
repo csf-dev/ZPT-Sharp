def872e142e094d293f9dff332541be2e96e5c6e
Craig Fowler <craig@craigfowler.me.uk>
2021-02-14 18:40:58 +0000

    Improve docs for the template directory class

--- ZptSharp.Abstractions/TemplateDirectory.cs
+++ ZptSharp.Abstractions/TemplateDirectory.cs
@@ -4,8 +4,20 @@ using System.IO;
 namespace ZptSharp
 {
     /// <summary>
-    /// A model which indicates a filesystem directory containing ZPT documents.
+    /// A model which represents a filesystem directory containing ZPT documents.
     /// </summary>
+    /// <remarks>
+    /// <para>
+    /// Each instance of this class represents a single filesystem directory.
+    /// This class has special handling within TALES path expressions, such that it may be treated much as
+    /// a filesystem path.
+    /// </para>
+    /// <para>
+    /// If a TALES path expression which makes use of an instance of this type references a document template
+    /// file, then that template will automatically be parsed, loaded and its macros made available via a
+    /// <c>macros</c> path property.
+    /// </para>
+    /// </remarks>
     public class TemplateDirectory
     {
         /// <summary>
@@ -15,7 +27,8 @@ namespace ZptSharp
         public string Path { get; }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="TemplateDirectory"/> class.
+        /// Initializes a new instance of the <see cref="TemplateDirectory"/> class
+        /// which represents the directory indicated by the specified string path.
         /// </summary>
         /// <param name="path">The path to the directory.</param>
         public TemplateDirectory(string path)
@@ -24,7 +37,8 @@ namespace ZptSharp
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="TemplateDirectory"/> class.
+        /// Initializes a new instance of the <see cref="TemplateDirectory"/> class
+        /// which represents the directory indicated by the specified <c>DirectoryInfo</c> instance.
         /// </summary>
         /// <param name="directory">A directory info object.</param>
         public TemplateDirectory(DirectoryInfo directory)
